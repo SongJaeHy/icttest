c8b4b1e892e4cc2f2d3cf1ba3456d5c7e7cb6a8f
송재현 <rnswothd@naver.com>
2021-07-31 08:13:16 +0900

    Add files via upload

---
+++ 07-javaoopgame/src/polymorphism/game/Monster.java
@@ -0,0 +1,41 @@
+package polymorphism.game;
+
+public class Monster {
+	private String name;
+	private int hp;
+	private int ap;
+	private int dp;
+	
+	public Monster(String name, int ap,int hp, int dp) {
+		this.name = name;
+		this.hp = hp;
+		this.ap = ap;
+		this.dp = dp;
+		
+	}
+	
+	public void doBattle(int uAtk) {
+		this.hp = (this.hp + this.dp) - uAtk;
+		if(this.hp <= 0) {
+			System.out.println(name + "몬스터는  죽었습니다.");
+		}
+	}
+	public String getName() {
+		return name;
+	}
+	public int getHp() {
+		return hp;
+	}
+	
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	
+	public int getAp() {
+		return ap;
+	}
+	
+	public int getDp() {
+		return dp;
+	}
+}
---
+++ 07-javaoopgame/src/polymorphism/game/Troll.java
@@ -0,0 +1,13 @@
+package polymorphism.game;
+
+public class Troll extends Monster{
+	public Troll() {
+		super("트롤",40,10,5);
+	}
+	public void doBattle(int uAtk) {
+		setHp(getHp() + getDp() - uAtk);
+		if(getHp() <= 0) {
+			System.out.println("트롤 몬스터는 죽었습니다.");
+		}
+	}
+}
